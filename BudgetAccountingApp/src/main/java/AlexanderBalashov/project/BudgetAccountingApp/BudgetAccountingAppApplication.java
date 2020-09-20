b6bc44c4fc65529259de2daadc79cdecd6607f34
Alexander Balashov <alaex777@gmail.com>
2020-09-20 18:28:34 +0300

    Created Spring App

---
+++ BudgetAccountingApp/src/main/java/AlexanderBalashov/project/BudgetAccountingApp/BudgetAccountingAppApplication.java
@@ -0,0 +1,13 @@
+package AlexanderBalashov.project.BudgetAccountingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetAccountingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetAccountingAppApplication.class, args);
+	}
+
+}
